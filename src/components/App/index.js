584cec6442a0069928e1d5ec55537ca19c94a760
just-boris <just-boris@hotmail.com>
2016-09-19 22:49:19 +0200

    state update logic

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,38 +1,44 @@
 import './styles.css';
-import React, { Component } from 'react';
+import React from 'react';
+import {compose, withState, mapProps} from 'recompose';
 import Slider from '../Slider';
 
-class App extends Component {
-  state = {
-    pointsLeft: 10,
-    skills: {
-      agility: 0,
-      charisma: 0,
-      strength: 0,
-      intellect: 0,
-      luck: 0
-    }
-  }
+const initialSkills = {
+  agility: 0,
+  charisma: 0,
+  strength: 0,
+  intellect: 0,
+  luck: 0
+};
+const maxAvailable = 10;
 
-  render() {
-    const {pointsLeft, skills} = this.state;
-    return (
-      <div className="App">
-        <h1 className="App__title">Spend your points</h1>
-        <div className="row">
-          <div className="row__col">
-            Choose wisely, points left:
-          </div>
-          <div className="row__col row__col_flex">
-              <input type="text" className="App__left" value={pointsLeft} />
-          </div>
+function App({available, skills, setSkills}) {
+  return (
+    <div className="App">
+      <h1 className="App__title">Spend your points</h1>
+      <div className="row">
+        <div className="row__col">
+          Choose wisely, points left:
         </div>
-        {Object.keys(skills).map(skill =>
-          <Slider key={skill} title={skill} value={skills[skill]} />
-        )}
+        <div className="row__col row__col_flex">
+            <input type="text" className="App__left" readOnly value={available} />
+        </div>
       </div>
-    );
-  }
+      {Object.keys(skills).map(skill =>
+        <Slider key={skill} title={skill} value={skills[skill]}
+          onChange={value => setSkills({...skills, [skill]: value})} />
+      )}
+    </div>
+  );
 }
 
-export default App;
+export default compose(
+  withState('skills', 'setSkills', initialSkills),
+  mapProps(({skills, ...props}) => {
+    const totalSpent = Object.keys(skills).reduce((total, skill) => total + skills[skill], 0);
+    return {
+      skills, ...props,
+      available: maxAvailable - totalSpent
+    }
+  })
+)(App);
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,11 +1,11 @@
 import './styles.css';
 import React from 'react';
 
-export default function Slider({title, value}) {
+export default function Slider({title, value, onChange}) {
   return (<label className="Slider row">
     <div className="row__col Slider__title">{title}</div>
     <div className="row__col row__col_flex">
-      <input type="range" className="Slider__control" value={value} />
+      <input type="range" max={10} className="Slider__control" value={value} onChange={e => onChange(+e.target.value)} />
     </div>
     <div className="row__col">
       {value} pts
