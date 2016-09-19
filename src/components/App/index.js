073ec34d9a21d7d0bba91cec790481a6c975a8cd
just-boris <just-boris@hotmail.com>
2016-09-19 22:24:21 +0200

    add design

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,33 @@ import React, { Component } from 'react';
 import Slider from '../Slider';
 
 class App extends Component {
+  state = {
+    pointsLeft: 10,
+    skills: {
+      agility: 0,
+      charisma: 0,
+      strength: 0,
+      intellect: 0,
+      luck: 0
+    }
+  }
+
   render() {
+    const {pointsLeft, skills} = this.state;
     return (
       <div className="App">
-        <h1>Adjust your scores</h1>
-        <Slider title="A" />
-        <Slider title="B" />
-        <Slider title="C" />
+        <h1 className="App__title">Spend your points</h1>
+        <div className="row">
+          <div className="row__col">
+            Choose wisely, points left:
+          </div>
+          <div className="row__col row__col_flex">
+              <input type="text" className="App__left" value={pointsLeft} />
+          </div>
+        </div>
+        {Object.keys(skills).map(skill =>
+          <Slider key={skill} title={skill} value={skills[skill]} />
+        )}
       </div>
     );
   }
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,9 +1,14 @@
 import './styles.css';
 import React from 'react';
 
-export default function Slider({title}) {
-    return (<label className="Slider">
-        {title}
-        <input type="range"/>
-    </label>);
+export default function Slider({title, value}) {
+  return (<label className="Slider row">
+    <div className="row__col Slider__title">{title}</div>
+    <div className="row__col row__col_flex">
+      <input type="range" className="Slider__control" value={value} />
+    </div>
+    <div className="row__col">
+      {value} pts
+    </div>
+  </label>);
 }
