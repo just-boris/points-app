bac304deca79ba48afbb4d14e5fd03c9cf09a0b1
just-boris <just-boris@hotmail.com>
2016-10-03 18:30:50 +0200

    special file for reducer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import React from 'react';
 import {compose, withReducer, mapProps} from 'recompose';
+import skillsReducer from './reducer';
 import Slider from '../Slider';
 
 function App({skills, available, updateSkills}) {
@@ -21,27 +22,6 @@ function App({skills, available, updateSkills}) {
       )}
     </div>
   );
-}
-
-const initialState = {
-  skills: {
-    agility: 0,
-    charisma: 0,
-    strength: 0,
-    intellect: 0,
-    stamina: 0
-  },
-  available: 10
-};
-
-export function skillsReducer(state = initialState, action) {
-  if(action.type === 'UPDATE') {
-    const delta = state.skills[action.skill] - action.value;
-    const available = state.available + delta;
-    const skills = {...state.skills, [action.skill]: action.value};
-    return available < 0 ? state : { skills, available };
-  }
-  return state;
 }
 
 export default compose(
